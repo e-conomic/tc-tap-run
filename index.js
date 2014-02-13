9f740268a2c7de3893b2fc961056728c39f7f846
Eduardo Sorribas <eduardo@sorribas.org>
2014-02-13 13:58:54 +0100

    Add escaping and error info.

--- index.js
+++ index.js
@@ -9,14 +9,29 @@ var child = cp.spawn(cli[0], cli.slice(1));
 
 child.stdout.pipe(tc);
 child.stdout.on('end', function() {
+	console.log(tc.results.list);
 	console.log("##teamcity[testSuiteStarted name='TAP test suite']");
 	tc.results.list.forEach(function(test) {
-		console.log("##teamcity[testStarted name='" + test.name + "' captureStandardOutput='true']");
+		console.log("##teamcity[testStarted name='" + escape(test.name) + "' captureStandardOutput='true']");
 		if (test.ok) {
-			console.log("##teamcity[testFinished name='" + test.name + "' ]");
+			console.log("##teamcity[testFinished name='" + escape(test.name) + "' ]");
 		} else {
-			console.log("##teamcity[testFailed name='" + test.name + "' captureStandardOutput='true']");
+			console.log("##teamcity[testFailed name='" + escape(test.name) + "' message='"+escape(test.stack.toString())+"' captureStandardOutput='true']");
 		}
 	});
 	console.log("##teamcity[testSuiteFinished name='TAP test suite']");
 });
+
+function escape(str) {
+	if (!str) return '';
+	return str
+		.replace(/\|/g, "||")
+		.replace(/\n/g, "|n")
+		.replace(/\r/g, "|r")
+		.replace(/\[/g, "|[")
+		.replace(/\]/g, "|]")
+		.replace(/\u0085/g, "|x")
+		.replace(/\u2028/g, "|l")
+		.replace(/\u2029/g, "|p")
+		.replace(/'/g, "|'");
+}
