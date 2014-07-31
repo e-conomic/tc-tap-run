8ede09cabd15e9c16b4d74cbc3e8939eb40d9ccb
Eduardo Sorribas <eduardo@sorribas.org>
2014-07-31 15:12:58 +0200

    Revert to run process and forward exit code.

--- index.js
+++ index.js
@@ -1,10 +1,14 @@
 #!/usr/bin/env node
 var fs = require('fs');
+var cp = require('child_process');
 var TapConsumer = require('tap').createConsumer;
 var tc = TapConsumer();
 
-process.stdin.pipe(tc);
-process.stdin.on('end', function() {
+var cli = process.argv[2].split(' ');
+var child = cp.spawn(cli[0], cli.slice(1));
+
+child.stdout.pipe(tc);
+child.stdout.on('end', function() {
 	console.log("##teamcity[testSuiteStarted name='TAP test suite']");
 	tc.results.list.forEach(function(test) {
 		console.log("##teamcity[testStarted name='" + escape(test.name) + "' captureStandardOutput='true']");
@@ -14,9 +18,10 @@ process.stdin.on('end', function() {
 			console.log("##teamcity[testFailed name='" + escape(test.name) + "' message='"+escape(test.stack.toString())+"' captureStandardOutput='true']");
 		}
 	});
-
 	console.log("##teamcity[testSuiteFinished name='TAP test suite']");
-	if (!tc.results.list.length) process.exit(1);
+});
+child.on('exit', function(code) {
+	process.exit(code);
 });
 
 function escape(str) {
